c18f463e25d80dc9022f4cfa473a7a5328efed4a
shiv2321 <130889263+shiv2321@users.noreply.github.com>
2025-02-24 14:12:41 +0530

    Updated dependencies

--- todo/settings.py
+++ todo/settings.py
@@ -50,7 +50,10 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 
 ROOT_URLCONF = 'todo.urls'
 
@@ -121,7 +124,7 @@ USE_TZ = True
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'todo/static')
+    os.path.join(BASE_DIR, 'static'),
 ]
 
 
